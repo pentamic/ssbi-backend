6143b7e9f1870ee5e3c082ca93e11fa04e440ed4
Dat Hoang <datht.it@live.com>
2017-09-27 11:57:31 +0700

    Add report and dashboard filter model

---
+++ Pentamic.SSBI/Models/Reporting/ReportFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pentamic.SSBI.Models.Reporting
+{
+    public class ReportFilter : IAuditable
+    {
+        public int Id { get; set; }
+        public int ReportId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FilterType { get; set; }
+        public string DataConfig { get; set; }
+
+        public Report Report { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTimeOffset ModifiedAt { get; set; }
+    }
+}
